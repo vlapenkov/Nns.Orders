3537da72d10dc91b8c3a30b653031b4b0ce3868e
Vladimir Lapenkov <vlapenkov@ya.ru>
2023-12-09 15:55:13 +0300

    fix: Поправил кодировку в тестах

--- Nns.Orders.UnitTests/Documents/WorkOrderServiceTests.cs
+++ Nns.Orders.UnitTests/Documents/WorkOrderServiceTests.cs
@@ -7,9 +7,9 @@ namespace Nns.Orders.UnitTests;
 
 public class WorkOrderServiceTests : OrderDbTestBase
 {
-    EquipmentState _equipmentState;
-    WorkCycleState _workState;
-    WorkOrderService _service;
+    private readonly EquipmentState _equipmentState;
+    private readonly WorkOrderService _service;
+    private readonly WorkCycleState _workState;
 
     public WorkOrderServiceTests()
     {
@@ -21,7 +21,7 @@ public class WorkOrderServiceTests : OrderDbTestBase
     protected override void PrepareData()
     {
         var applications = new[]
-          {
+        {
             //01.01 
             new EquipmentApplication
                 {StartDate = new DateTime(2020, 1, 1), WorkTypeId = 1, EquipmentTypeId = 1, IsActive = true},
@@ -30,7 +30,7 @@ public class WorkOrderServiceTests : OrderDbTestBase
 
             //01.02
             new EquipmentApplication
-                {StartDate = new DateTime(2020, 1, 2), WorkTypeId = 2, EquipmentTypeId = 2, IsActive = false},            
+                {StartDate = new DateTime(2020, 1, 2), WorkTypeId = 2, EquipmentTypeId = 2, IsActive = false},
 
             //01.02
             new EquipmentApplication
@@ -40,16 +40,19 @@ public class WorkOrderServiceTests : OrderDbTestBase
 
         var workCycles = new[]
         {
-            new WorkCycle {Id=1,StartDate = new DateTime(2020, 1, 1), WorkTypeId = 1, OrderNumber = 1, IsActive = true},
-            new WorkCycle {Id=2,StartDate = new DateTime(2020, 1, 1), WorkTypeId = 2, OrderNumber = 2, IsActive = true},
-            new WorkCycle {Id=3,StartDate = new DateTime(2020, 1, 1), WorkTypeId = 3, OrderNumber = 3, IsActive = true},
+            new WorkCycle
+                {Id = 1, StartDate = new DateTime(2020, 1, 1), WorkTypeId = 1, OrderNumber = 1, IsActive = true},
+            new WorkCycle
+                {Id = 2, StartDate = new DateTime(2020, 1, 1), WorkTypeId = 2, OrderNumber = 2, IsActive = true},
+            new WorkCycle
+                {Id = 3, StartDate = new DateTime(2020, 1, 1), WorkTypeId = 3, OrderNumber = 3, IsActive = true},
 
-            // на 02.01 отменили 
-            new WorkCycle {Id=4,StartDate = new DateTime(2020, 1, 2), WorkTypeId = 2, OrderNumber = 2, IsActive = false},
+            // пїЅпїЅ 02.01 пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ 
+            new WorkCycle
+                {Id = 4, StartDate = new DateTime(2020, 1, 2), WorkTypeId = 2, OrderNumber = 2, IsActive = false},
 
-            new WorkCycle {Id=5,StartDate = new DateTime(2020, 1, 2), WorkTypeId = 4, OrderNumber = 4, IsActive = false},
-
-
+            new WorkCycle
+                {Id = 5, StartDate = new DateTime(2020, 1, 2), WorkTypeId = 4, OrderNumber = 4, IsActive = false}
         };
 
         _db.WorkCycles.AddRange(workCycles);
@@ -59,69 +62,69 @@ public class WorkOrderServiceTests : OrderDbTestBase
 
     [Fact]
     public async Task AddWorkOrders_ShouldBeCreated()
-    {            
-
+    {
         var model = new WorkOrder
         {
-            StartDate = new DateTime(2020, 1, 1), ExcavationId = 1, WorkTypeId = 1, EquipmentTypeId = 1,OrderNumber = 3, Value = 10
+            StartDate = new DateTime(2020, 1, 1), ExcavationId = 1, WorkTypeId = 1, EquipmentTypeId = 1,
+            OrderNumber = 3, Value = 10
         };
 
         await _service.Add(model);
 
         _db.WorkOrders.Should().ContainSingle();
-        
+
 
         model = new WorkOrder
         {
-            StartDate = new DateTime(2020, 1, 1),ExcavationId = 1,WorkTypeId = 2,EquipmentTypeId = 2, OrderNumber = 7,Value = 10
+            StartDate = new DateTime(2020, 1, 1), ExcavationId = 1, WorkTypeId = 2, EquipmentTypeId = 2,
+            OrderNumber = 7, Value = 10
         };
 
         await _service.Add(model);
 
-        _db.WorkOrders.Should().HaveCount(c => c == 2);       
+        _db.WorkOrders.Should().HaveCount(c => c == 2);
     }
 
     [Fact]
     public async Task AddWorkOrder_CantApply_ShouldFail()
     {
-
         var model = new WorkOrder
         {
             StartDate = new DateTime(2020, 1, 1),
             ExcavationId = 1,
             WorkTypeId = 2,
-            EquipmentTypeId = 2, // применяемость есть на 01.01
+            EquipmentTypeId = 2, //  01.01
             OrderNumber = 3,
             Value = 10
         };
 
-        await _service.Add(model);        
+        await _service.Add(model);
 
         model = new WorkOrder
         {
             StartDate = new DateTime(2020, 1, 2),
             ExcavationId = 1,
             WorkTypeId = 2,
-            EquipmentTypeId = 2, // применяемости нет на 02.01
+            EquipmentTypeId = 2, //  02.01
             OrderNumber = 3,
             Value = 10
         };
 
-        Func<Task<long>> funcTask = (async () => await _service.Add(model));
+        Func<Task<long>> funcTask = async () => await _service.Add(model);
 
-        await funcTask.Should().ThrowAsync<AppException>().WithMessage("Применяемость не активна WorkTypeId=2 EquipmentTypeId=2");
+        await funcTask.Should().ThrowAsync<AppException>()
+            .WithMessage("РџСЂРёРјРµРЅСЏРµРјРѕСЃС‚СЊ РЅРµ Р°РєС‚РёРІРЅР° WorkTypeId=2 EquipmentTypeId=2");
     }
 
     [Fact]
     public async Task AddWorkOrders_MeetOrder_ShouldSucceed()
     {
-
         var model = new WorkOrder
         {
             StartDate = new DateTime(2020, 1, 3),
             ExcavationId = 1,
             WorkTypeId = 1,
-            EquipmentTypeId = 1, 
+            EquipmentTypeId = 1,
             OrderNumber = 1,
             Value = 10
         };
@@ -134,19 +137,19 @@ public class WorkOrderServiceTests : OrderDbTestBase
             StartDate = new DateTime(2020, 1, 3),
             ExcavationId = 1,
             WorkTypeId = 3,
-            EquipmentTypeId = 3, 
+            EquipmentTypeId = 3,
             OrderNumber = 3,
             Value = 10
-        };               
+        };
 
         await _service.Add(model);
 
         _db.WorkOrders.Should().HaveCount(c => c == 2);
     }
 
+    [Fact]
     public async Task AddWorkOrders_DoenntMeetOrder_ShouldFail()
     {
-
         var model = new WorkOrder
         {
             StartDate = new DateTime(2020, 1, 3),
@@ -170,8 +173,9 @@ public class WorkOrderServiceTests : OrderDbTestBase
             Value = 10
         };
 
-        Func<Task<long>> funcTask = (async () => await _service.Add(model));
+        Func<Task<long>> funcTask = async () => await _service.Add(model);
 
-        await funcTask.Should().ThrowAsync<AppException>().WithMessage("Порядок видов работ в плане не соответствует порядку в активном производственном цикле");
+        await funcTask.Should().ThrowAsync<AppException>()
+            .WithMessage("РџРѕСЂСЏРґРѕРє РІРёРґРѕРІ СЂР°Р±РѕС‚ РІ РїР»Р°РЅРµ РЅРµ СЃРѕРѕС‚РІРµС‚СЃС‚РІСѓРµС‚ РїРѕСЂСЏРґРєСѓ РІ Р°РєС‚РёРІРЅРѕРј РїСЂРѕРёР·РІРѕРґСЃС‚РІРµРЅРЅРѕРј С†РёРєР»Рµ");
     }
 }
